9107acfbbe83bcec11517174126b4bc8c4a6920a
Gordon <gb@gordonbailey.net>
2014-04-01 16:03:16 -0400

    Updated wishlist stuff, working pretty well now

--- home/shopping_cart.py
+++ home/shopping_cart.py
@@ -27,8 +27,14 @@ def remove(request):
 
          cart.courses.remove(course)
 
-
    return redirect("/shopping-cart")
+
+
+def get_cart_dict(cart):
+   courses = cart.courses.values('course_id', 'course_code', 'course_name')
+
+   return HttpResponse(json.dumps(list(courses)), content_type="applicationn/json")
+
 
 @login_required
 def add(request):
@@ -40,19 +46,16 @@ def add(request):
       cart.courses.add(course)
       cart.save()
 
-      courses = cart.courses.values_list('course_id', flat=True)
-
-      return HttpResponse(json.dumps(list(courses)), content_type="applicationn/json")
+      return get_cart_dict(cart)
    else:
       return HttpResponse("[]", content_type="application/json")
+
 
 def get_cart(request):
     if request.user.is_authenticated():
         cart, created = ShoppingCart.objects.get_or_create(user=request.user)
 
-        courses = cart.courses.values_list('course_id', flat=True)
-
-        return HttpResponse(json.dumps(list(courses)), content_type="applicationn/json")
+        return get_cart_dict(cart)
     else:
         return HttpResponse("[]", content_type="application/json")
    
--- home/static/js/shopping-cart.js
+++ home/static/js/shopping-cart.js
@@ -1,14 +1,49 @@
 $(document).ready(function(){
 
-   var updateCart = function(n_items){
-      $("#shopping-cart-badge").text(n_items);
+   var updateBadge = function(nItems){
+      $("#shopping-cart-badge").text(nItems);
+   }
+
+   var updateSidebar = function(cart){
+      var table = $("#wishlist-sidebar");
+      table.empty();
+
+      if(cart.length > 0){
+         $.each(cart, function(i, course){
+            table.append(
+               '<tr><td><b>'
+               + course['course_code']
+               + '</b> - '
+               + course['course_name']
+               + '</td></tr>');
+         });
+      }else{
+         table.append('<tr><td>No courses currently added</td></tr>');
+      }
+   }
+
+   var setButtonChecked = function(btn){
+      btn
+         .button('complete')
+         .removeClass('btn-primary')
+         .addClass('btn-success');
+      
+   }
+
+   var updateButtons = function(cart){
+      $.each(cart, function(i, course){
+         var btn = $("#shopping-btn-" + course.course_id); 
+         setButtonChecked(btn);
+      });
    }
 
    $.ajax("/shopping-cart/get-cart",
       {
          dataType:"json",
          success:function(data){
-            updateCart(data.length);
+            updateBadge(data.length);
+            updateSidebar(data);
+            updateButtons(data);
             // TODO - disable buttons of items already in cart
          }
       });
@@ -25,11 +60,9 @@ $(document).ready(function(){
             dataType:"json",
             data:{ course : btn.data("id") },
             success:function(data, textStatus, jqXHR){
-               btn
-                  .button('reset')
-                  .removeClass('btn-primary')
-                  .addClass('btn-success');
-               updateCart(data.length);
+               setButtonChecked(btn);
+               updateBadge(data.length);
+               updateSidebar(data);
             },
             error:function(jqXHR, textStatus, errorThrown){
                btn
